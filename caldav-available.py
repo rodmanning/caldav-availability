d830059b563d3fcb243c19410fa29683bc17b881
Rod Manning <rod.t.manning@gmail.com>
2017-02-24 20:47:24 +0930

    Add create_blocks() function
    This commit added detail to the create_blocks function which is used to
    populate the list of blocks that we're compiling free/busy time for.
    
    This function is a fairly simple iterator that keeps adding blocks until
    > the nominated end datetime for the calendar that's being generated.
    
    The function also does a bit of trimming to set the last block in the day
    to the last available hour in the day.
    
    Couple of other minor changes:
    
    - Put the program logic above the variables in the main docstring (seems
      to make more sense to know how it works before working out what variables
      are required).
    
    - Changed the "day_start" and "day_end" args to ints (e.g. "06" = 6 AM)
    
    - Changed the Event and Block object __init__ functions to use **kwargs
      instead of named args (bit more flexible).
    
    - Renamed generic lists used to hold things as "stack" (consistency :) )
    
    - Added code to convert args that should be int to actually be int.
    
    Signed-off-by: Rod Manning <rod.t.manning@gmail.com>

--- caldav-available.py
+++ caldav-available.py
@@ -5,8 +5,28 @@ import argparse
 import urllib.request
 import pytz
 import datetime
+import math
 
 """Compute free/busy information from a CalDAV file.
+
+Logic of program is as follows:
+
+1. Download CalDAV file from server (requires authentication)
+
+2. Process the text of the CalDAV file to create a list of all published
+   'event' objects within the date range specified (i.e. between start_date and
+   end_date).
+
+3. Create a list of all blocks within the date range specified.
+
+4. Search the list of events and assign events to each block if they fall
+   within the block.
+
+5. For each block, compute the time assigned to events in that block to
+  calculate the free/busy time for the block.
+
+6. Assign a classification to the block to indicate if it has high (75 <= n),
+   moderate (30 < n < 75), or low (n <= 30 ) time allocated.
 
 Inputs are as follows:
 
@@ -38,38 +58,19 @@ block_length:
   Length (in hours) of blocks that each day is divided into when showing
   free/busy information. Defaults to 6 hours.
 
-Logic of program is as follows:
-
-1. Download CalDAV file from server (requires authentication)
-
-2. Process the text of the CalDAV file to create a list of all published
-   'event' objects within the date range specified (i.e. between start_date and
-   end_date).
-
-3. Create a list of all blocks within the date range specified.
-
-4. Search the list of events and assign events to each block if they fall
-   within the block.
-
-5. For each block, compute the time assigned to events in that block to
-  calculate the free/busy time for the block.
-
-6. Assign a classification to the block to indicate if it has high (75 <= n),
-   moderate (30 < n < 75), or low (n <= 30 ) time allocated.
-
 """
 
 ARG_DEFAULTS = {
     "start": datetime.datetime.now(),
     "end": (datetime.datetime.now() + datetime.timedelta(days=14)),
-    "day_start": "0600",     # 0600 local time
-    "day_end": "2200",      # 2200 local time
+    "day_start": "06",     # 0600 local time
+    "day_end": "22",      # 2200 local time
     "block_length": "6",  # Hours
     "timezone": "Australia/Darwin"  # Default local timezone
 }
 
 
-class event(object):
+class Event(object):
     """An event extracted from a CalDAV calendar entry.
 
     Each ``event`` has the following properties:
@@ -91,7 +92,7 @@ class event(object):
 
     """
 
-    def __init__(self, event_data):
+    def __init__(self, **kwargs):
         """Initialize an Event object.
 
         event_data:
@@ -99,11 +100,11 @@ class event(object):
           the event.
 
         """
-        self.uid = event_data["UID"]
-        self.name = event_data["SUMMARY"]
-        self.start_dt = event_data["DTSTART"]
-        self.end_dt = event_data["DTEND"]
-        self.categories = event_data["CATEGORIES"].split(" ")
+        self.uid = kwargs["UID"]
+        self.name = kwargs["SUMMARY"]
+        self.start_dt = kwargs["DTSTART"]
+        self.end_dt = kwargs["DTEND"]
+        self.categories = kwargs["CATEGORIES"].split(" ")
 
     def __str__(self):
         return "{0} ({1})".format(
@@ -112,14 +113,33 @@ class event(object):
         )
 
 
-class block(object):
+class Block(object):
     """A block of time in a calendar.
 
     Blocks are of a defined length and are the units of time that are used to
     calculate the free/busy information for display in a calendar.
 
     """
-    pass
+
+    def __init__(self, **kwargs):
+        """Initialize the Block object.
+
+        start_dt:
+          A datetime object representing the start of the block.
+
+        end_dt:
+          A datetime object representing the end of the block.
+
+        Note that when the block is initialized the free time is set to the
+        length of the block, and the busy time is set to zero (0).
+        """
+        self.start = kwargs["start_dt"]
+        self.end = kwargs["end_dt"]
+        self.length = self.end - self.start
+        self.end_dt = self.start + self.length
+        self.busy = datetime.timedelta(minutes=0)
+        self.free = self.length
+        self.classes = []
 
 
 def get_calendar(username, password, url, realm):
@@ -214,16 +234,53 @@ def process_cal_data(cal_data, start=None, end=None,
 def create_events(calendar_file, start, end):
     """Create events from data contained in a CalDAV file."""
     calendar_data = process_cal_data(calendar_file, start, end)
-    events = []   # List to hold the events
+    stack = []   # List to hold the events
     for item in calendar_data.values():
-        evt = event(item)
-        events.append(evt)
-    return events
+        evt = Event(**item)
+        stack.append(evt)
+    return stack
 
 
-def create_blocks(start_date, end_date, start_hours, end_hours, block_length):
+def create_blocks(start_dt, end_dt, start_hour, end_hour, block_length):
     """Create the blocks used to calculate free/busy time."""
-    pass
+    stack = []
+    # Get the start time for the first block
+    blocks_per_day = math.ceil((end_hour - start_hour) / block_length)
+    block_start = start_dt.replace(
+        hour=start_hour,
+        minute=0, second=0, microsecond=0
+    )
+    block_length = datetime.timedelta(hours=block_length)
+    block_end = block_start + block_length
+    day_end = block_start.replace(hour=end_hour)
+    end_of_period = end_dt.replace(
+        hour=end_hour,
+        minute=0, second=0, microsecond=0
+    )
+    # Create blocks for each day until calculated end_hours > end_hours
+    # specified. When this happens, trim the end hours and increment to the
+    # next day
+    while block_start < end_of_period:
+        # Test for incrementing to the next day
+        if block_start > day_end:
+            block_start = block_start.replace(hour=start_hour)
+            block_start = block_start + datetime.timedelta(hours=24)
+            block_end = block_start + block_length
+            day_end = block_start.replace(hour=end_hour)
+        # Test to set end of block to last hour in daily calendar
+        if block_end > day_end:
+            block_end = block_start.replace(hour=end_hour)
+        data = {
+            "start_dt": block_start,
+            "end_dt": block_end,
+        }
+        stack.append(Block(**data))
+        # Increment the block_start another n hours, then do the same for
+        # the block_end. Note that because we update the block_start first,
+        # The block end is incremented based onthe *new* starting dt :)
+        block_start = block_start + block_length
+        block_end = block_start + block_length
+    return stack
 
 
 def assign_events(events, blocks):
@@ -315,6 +372,10 @@ def parse_args():
         # Localize to the selected local time
         tz = pytz.timezone(ARG_DEFAULTS["timezone"])
         vars(args)[item] = tz.localize(vars(args)[item])
+    # Convert the various hours values to int's
+    args.block_length = int(args.block_length)
+    args.day_start = int(args.day_start)
+    args.day_end = int(args.day_end)
     return args
 
 
@@ -326,3 +387,6 @@ if __name__ == "__main__":
     # End test block
     # cal_data = get_calendar(args.username, args.password, args.url, args.realm)
     events = create_events(cal_data, args.start, args.end)
+    blocks = create_blocks(
+        args.start, args.end, args.day_start, args.day_end, args.block_length
+    )
